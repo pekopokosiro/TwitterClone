faad1d293cfe2ebf74c29752487de61fa58d4aa9
石倉大輔 <ishikuradaisuke@ishikuradaisukenoMacBook-Air.local>
2021-07-13 10:44:36 +0900

    その他の画面

--- Views/sign-in.php
+++ Views/sign-in.php
@@ -1,33 +1,31 @@
+<?php
+//設定関連を読み込む
+include_once('../config.php');
+//便利な関数を読み込む
+include_once('../util.php');
+?>
 <!DOCTYPE html>
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="icon" href="/TwitterClone/Views/img/logo-twitterblue.svg">
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link href="/TwitterClone/Views/css/style.css" rel="stylesheet">
-
+    <?php include_once('../views/common/head.php'); ?>
     <title>ログイン画面 / Twitterクローン</title>
     <meta name="description" content="ログイン画面です">
 </head>
 
 <body class="sign-up text-center">
-<main class="form-sign-up">
-    <form action="sign-in.php" method="post">
-        <img src="/TwitterClone/Views/img/logo-white.svg" alt="" class="logo-white">
-        <h1>Twitterクローンにログイン</h1>
-        <input type="e-mail" class="form-control" name="e-mail" placeholder="メールアドレス"  required autofocus>
-        <input type="password" class="form-control" name="password" placeholder="パスワード"  required>
-        <button class="w-100 btn btn-lg"　type="submit">ログイン</button>
-        <p class="mt-3 mb-2"><a href="sign-up.php">会員登録する</a></p>
-        <p class="mt-2 mb-3 text-muted">&copy; 2021</p>
-    </form>
-</main>
-
+    <main class="form-sign-up">
+        <form action="sign-in.php" method="post">
+            <img src="/TwitterClone/Views/img/logo-white.svg" alt="" class="logo-white">
+            <h1>Twitterクローンにログイン</h1>
+            <input type="e-mail" class="form-control" name="e-mail" placeholder="メールアドレス" required autofocus>
+            <input type="password" class="form-control" name="password" placeholder="パスワード" required>
+            <button class="w-100 btn btn-lg" 　type="submit">ログイン</button>
+            <p class="mt-3 mb-2"><a href="sign-up.php">会員登録する</a></p>
+            <p class="mt-2 mb-3 text-muted">&copy; 2021</p>
+        </form>
+    </main>
+    <?php include_once('../Views/common/foot.php'); ?>
 </body>
 
 
