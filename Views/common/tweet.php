0eca3f321ff2afcbf575ea0d3b0d43a03a23d99e
石倉大輔 <ishikuradaisuke@ishikuradaisukenoMacBook-Air.local>
2021-07-20 01:32:11 +0900

    非同期でいいね！

--- Views/common/tweet.php
+++ Views/common/tweet.php
@@ -1,6 +1,6 @@
 <div class="tweet">
     <div class="user">
-        <a href="profile.php?user_id=1">
+        <a href="profile.php?user_id=<?php echo htmlspecialchars($view_tweet['user_id']); ?>">
             <img src="<?php echo buildImagePath($view_tweet['user_image_name'], 'user'); ?>" alt="">
         </a>
     </div>
@@ -11,15 +11,15 @@
                 <span class="user-name">@<?php echo htmlspecialchars($view_tweet['user_name']); ?> ・<?php echo convertToDayTimeAgo($view_tweet['tweet_created_at']); ?></span>
             </a>
         </div>
- 
+
         <p><?php echo htmlspecialchars($view_tweet['tweet_body']); ?></p>
- 
+
         <?php if (isset($view_tweet['tweet_image_name'])) : ?>
             <img src="<?php echo buildImagePath($view_tweet['tweet_image_name'], 'tweet'); ?>" alt="" class="post-image">
         <?php endif; ?>
- 
+
         <div class="icon-list">
-            <div class="like js-like" data-like-id="<?php echo htmlspecialchars($view_tweet['like_id']); ?>">
+            <div class="like js-like" data-tweet-id="<?php echo htmlspecialchars($view_tweet['tweet_id']); ?>" data-like-id="<?php echo htmlspecialchars($view_tweet['like_id']); ?>">
                 <?php
                 if (isset($view_tweet['like_id'])) {
                     // いいね！している場合
