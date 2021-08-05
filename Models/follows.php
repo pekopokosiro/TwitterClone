094d7edb196620f154089385d9cf91f31b46a3f8
石倉大輔 <ishikuradaisuke@ishikuradaisukenoMacBook-Air.local>
2021-08-05 20:38:54 +0900

    ホーム画面完成

--- Models/follows.php
+++ Models/follows.php
@@ -2,7 +2,7 @@
 ///////////////////////////////////////
 // フォローデータを処理
 ///////////////////////////////////////
- 
+
 /**
  * フォローを作成
  *
@@ -17,16 +17,16 @@ function createFollow(array $data)
         echo 'MySQLの接続に失敗しました。：' . $mysqli->connect_error . "\n";
         exit;
     }
- 
+
     // ------------------------------------
     // SQLクエリを作成（新規登録）
     // ------------------------------------
     $query = 'INSERT INTO follows(follow_user_id, followed_user_id) VALUES (?, ?)';
     $statement = $mysqli->prepare($query);
- 
+
     // プレースホルダに値をセット
     $statement->bind_param('ii', $data['follow_user_id'], $data['followed_user_id']);
- 
+
     // ------------------------------------
     // 戻り値を作成
     // ------------------------------------
@@ -39,17 +39,17 @@ function createFollow(array $data)
         $response = false;
         echo 'エラーメッセージ：' . $mysqli->error . "\n";
     }
- 
+
     // ------------------------------------
     // 後処理
     // ------------------------------------
     // DB接続を開放
     $statement->close();
     $mysqli->close();
- 
+
     return $response;
 }
- 
+
 /**
  * フォローを取り消し
  *
@@ -64,36 +64,92 @@ function deleteFollow(array $data)
         echo 'MySQLの接続に失敗しました。：' . $mysqli->connect_error . "\n";
         exit;
     }
- 
+
     // 更新日時を追加
     $data['updated_at'] = date('Y-m-d H:i:s');
- 
+
     // ------------------------------------
     // SQLクエリを作成
     // ------------------------------------
     // 論理削除のクエリを作成
     $query = 'UPDATE follows SET status = "deleted", updated_at = ? WHERE id = ? AND follow_user_id = ?';
     $statement = $mysqli->prepare($query);
- 
+
     // プレースホルダに値をセット
     $statement->bind_param('sii', $data['updated_at'], $data['follow_id'], $data['follow_user_id']);
- 
+
     // ------------------------------------
     // 戻り値を作成
     // ------------------------------------
     $response = $statement->execute();
- 
+
     // SQLエラーの場合->エラー表示
     if ($response === false) {
         echo 'エラーメッセージ：' . $mysqli->error . "\n";
     }
- 
+
     // ------------------------------------
     // 後処理
     // ------------------------------------
     // DB接続を開放
     $statement->close();
     $mysqli->close();
- 
+
     return $response;
-} 
+}
+
+
+/**
+ * 自分がフォーローしているユーザーID一覧を取得
+ *
+ * @param int $follow_user_id
+ * @return array|false
+ */
+function findFollowingUserIds(int $follow_user_id)
+{
+    // DB接続
+    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    if ($mysqli->connect_errno) {
+        echo 'MySQLの接続に失敗しました。：' . $mysqli->connect_error . "\n";
+        exit;
+    }
+
+    // エスケープ
+    $follow_user_id = $mysqli->real_escape_string($follow_user_id);
+
+    // ------------------------------------
+    // SQLクエリを作成
+    // ------------------------------------
+    $query = 'SELECT followed_user_id FROM follows'
+        . ' WHERE status = "active" AND follow_user_id ="' . $follow_user_id . '"';
+
+    // ------------------------------------
+    // 戻り値を作成
+    // ------------------------------------
+    $result = $mysqli->query($query);
+
+    // SQLエラーの場合->エラー表示
+    if (!$result) {
+        echo 'エラーメッセージ：' . $mysqli->error . "\n";
+        // DB接続を開放
+        $mysqli->close();
+        return false;
+    }
+
+    // フォロー一覧を取得
+    $follows = $result->fetch_all(MYSQLI_ASSOC);
+
+    // ユーザーIDの一覧を作成
+    $following_user_ids = [];
+    foreach ($follows as $follow) {
+        $following_user_ids[] = $follow['followed_user_id'];
+    }
+
+    // ------------------------------------
+    // 後処理
+    // ------------------------------------
+    // DB接続を開放
+    $mysqli->close();
+
+    return $following_user_ids;
+}
--- Views/home.php
+++ Views/home.php
@@ -20,8 +20,8 @@
                     <img src="<?php echo htmlspecialchars($view_user['image_path']); ?>" alt="">
                 </div>
                 <div class="input-area">
-                    <form action="post.php" method="post" enctype="multipart/form-data">
-                        <textarea name="body" placeholder="いまどうしてる？" maxlength="140"></textarea>
+                    <form action="post.php" method="post" enctype="multipart/form-data" >
+                        <textarea name="body" placeholder="いまどうしてる？" maxlength="140" ></textarea>
                         <div class="bottom-area">
                             <div class="mb-0">
                                 <input type="file" name="image" class="form-control form-control-sm">
