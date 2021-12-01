9e0ddf5d4b8f94dac18a1dfaf7215067c117cffe
pekopokosiro <soniccat01@gmail.com>
2021-12-01 11:31:26 +0900

    n

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-// include_once(__DIR__.'Controllers/home.php');
-include_once('config.php');
-header('Location:' . HOME_URL . 'Controllers/sign-up.php');
+chdir("Controllers");
+include_once('home.php');
 ?>
