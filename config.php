4c2a143054a13d8dffc8f77df19a44f11e77bf2e
pekopokosiro <soniccat01@gmail.com>
2021-12-07 11:47:23 +0900

    mysql

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ date_default_timezone_set('Asia/Tokyo');
 // URL/ディレクトリ設定
 define('HOME_URL', '/');
 // データベースの接続情報
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'CLEARDB_DATABASE_URL');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'b17b26331719ad:08928e37@us-cdbr-east-04.cleardb.com/heroku_c2b349a1af63846');
 define('DB_NAME', 'twitter_clone');
