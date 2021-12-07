c645aff21ff4f37babca85d6b0226dcfa8cb4dc4
pekopokosiro <soniccat01@gmail.com>
2021-12-07 23:15:11 +0900

    test

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ date_default_timezone_set('Asia/Tokyo');
 // URL/ディレクトリ設定
 define('HOME_URL', '/');
 // データベースの接続情報
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_NAME', 'twitter_clone');
+define('DB_HOST', 'twitterclone.pekopoko.tokyo');
+define('DB_USER', 'peko1112poko_wp3');
+define('DB_PASSWORD', 'tw12072304');
+define('DB_NAME', 'peko1112poko_wp3');
