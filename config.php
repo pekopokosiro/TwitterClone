3146e25e1f2b13dd5f32ef5f457bab2417e7eab9
pekopokosiro <soniccat01@gmail.com>
2021-12-07 11:07:03 +0900

    test

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 // 日本時間にする
 date_default_timezone_set('Asia/Tokyo');
 // URL/ディレクトリ設定
-define('HOME_URL', '/TwitterClone/');
+define('HOME_URL', '/');
 // データベースの接続情報
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
